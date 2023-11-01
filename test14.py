fcb1265399f42edd2be6325d289d108831c1bb9e
aaaNhy <90199362+aaaNhy@users.noreply.github.com>
2023-11-01 23:46:55 +0800

    Update test14.py

--- test14.py
+++ test14.py
@@ -5,7 +5,7 @@ from streamlit_pandas_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
 
-df = pd.read_excel('https://github.com/aaaNhy/Intelligent-metallurgy/testB_2022-09-19.xlsx')
+df = pd.read_excel('https://github.com/aaaNhy/Intelligent-metallurgy.git/testB_2022-09-19.xlsx')
 
 pr = df.profile_report()
 st_profile_report(pr)
