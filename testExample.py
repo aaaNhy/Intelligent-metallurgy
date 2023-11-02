91822ca47eef7293d9d3938ad657ae25eb0169f1
aaaNhy <90199362+aaaNhy@users.noreply.github.com>
2023-11-02 12:56:58 +0800

    Update testExample.py

--- testExample.py
+++ testExample.py
@@ -12,15 +12,12 @@ if st.button('你要做页面吗'):
 else:
      st.write('是的')
 
-# 样例 1
 
 st.write('这是输出语句，可以输出各种各样的数据')
 
-# 样例 2
 
 st.write("int:", 1234)
 
-# 样例 3
 
 df = pd.DataFrame({
     '身高cm': [160, 170, 180, 190],
@@ -28,15 +25,10 @@ df = pd.DataFrame({
 })
 st.write(df)
 
-# 样例 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# 样例 5
 
 df2 = pd.DataFrame(
     np.random.randn(400, 3),
-    columns=['身高', '体重', '肤色'])
+    columns=['a', 'b', 'c'])
 c = alt.Chart(df2).mark_circle().encode(
-    x='身高', y='体重', size='肤色', color='肤色', tooltip=['身高', '体重', '肤色'])
-st.write(c)
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write("图表：", c)
