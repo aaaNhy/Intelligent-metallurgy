7bf34ccad413b5305b58bf2c526365e1bde930ff
aaaNhy <90199362+aaaNhy@users.noreply.github.com>
2023-11-02 13:21:41 +0800

    Update testExample.py

--- testExample.py
+++ testExample.py
@@ -48,14 +48,6 @@ values = st.slider(
 st.write('Values:', values)
 
 
-st.subheader('时间范围滑动条：')
-
-appointment = st.slider(
-     "制定你的时间表:",
-     value=(time(11, 30), time(12, 45)))
-st.write("你指定的时间表是:", appointment)
-
-st.subheader('日期滑动条')
 
 start_time = st.slider(
      "什么时候开始?",
